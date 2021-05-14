30a832b56341f3b7760911c5765230052a0c01d2
Daniel Hamidi <hamidiwebdesign@gmail.com>
2021-05-14 13:46:02 -0700

    initial commit building html structure

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+//let mesy = 'hello javascript';
+// const chngeee = 'changed the name of the source JS file to min';
+// alert(chngeee);
+//alert(mesy);
+//console.log('Now it works');
+//console.log('Now it works');
