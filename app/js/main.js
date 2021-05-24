c10486a5350630305f8d52bdf1a4c9a172a5dd5e
Daniel Hamidi <hamidiwebdesign@gmail.com>
2021-05-24 15:50:59 -0700

    cleaning up the code from redundant comments; changing normal functions to arrow functions; moving timer function inside setInterval function; capturing and fixing the latest timer count

--- app/js/main.js
+++ app/js/main.js
@@ -1,164 +1,135 @@
-
 const resetWholeGeneration = () => {
-    window.location.reload();
-}
+  window.location.reload();
+};
 let result;
 let binar = [];
 let final = "";
 let decimVal = 0;
-for (let i = 0; i < 8; i++) {
-    result = Math.round(Math.random(1, 2));
-    binar.push(result);
-    // console.log(binar);
-    final = final.concat(result);
+let clickedObjectContainer;
+let myChosenAnswers = ""; //variable for keeping concatenated words
 
+let givenAns = document.getElementById("combAns");
+for (let i = 0; i < 8; i++) {
+  //Generating random binary number
+  result = Math.round(Math.random(1, 2));
+  binar.push(result);
+  final = final.concat(result);
 }
 let binaryVlaue = document.getElementById("binary-Value");
 
-binaryVlaue.innerHTML = final;
-//convert to decial value
+binaryVlaue.innerHTML = final; //displaying the random binary number on the page
+
 for (let i = 0; i < binar.length; i++) {
-    if (binar[i] == 1) {
-        decimVal = decimVal + Math.pow(2, 7 - i);
-        // console.log(Math.pow(2, 7 - i));
-        // console.log(decimVal);
-    } else {
-        decimVal += 0;
-    }
-    // console.log(decimVal);
-}
-console.log(decimVal);
-console.log(final);
-
-/////////////////////////////////////////////////////////////************* */
-////////////////////////////////////////////////////////////************************* */
-/////////////////////////////////////////////////////////********************************************************** */
-
-/*//////////VARIABLES▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄*/
-let clickedObjectContainer;
-let myChosenAnswers = ''; //variable for keeping concatenated words
-let givenAns = document.getElementById('combAns');
-//~~~~~~~~~~~~~~~~~asked question loop~~~~~~~~~~~~~~~~
-/*//////////FUNCTIONS/////////*/
-function makeItHidden() {
-    //function for hidding the clicked cell table row
-    clickedObjectContainer.style.display = 'none';
-} //hiding
-
-function testForHidingRow() {
-    let tablRowCount = document.querySelector('.answersTable').rows.length;
-    console.log(tablRowCount);
-    for (var Indx = 1; Indx < tablRowCount + 1; Indx++) {
-        if (
-            document.querySelector(
-                '.answersTable tr:nth-child(' + Indx + ') td:nth-child(2)>a'
-            ).style.display == 'none' &&
-            document.querySelector(
-                '.answersTable tr:nth-child(' + Indx + ') td:nth-child(3)>a'
-            ).style.display == 'none'
-        ) {
-            document.querySelector(
-                '.answersTable tr:nth-child(' + Indx + ')'
-            ).style.display = 'none';
-        } //end of if
-    } //end of loop
-} //end of function hiding rows
-
-function populatGivenAns() {
-    if (myChosenAnswers == '') {
-        myChosenAnswers = clickedObjectContainer.querySelector('div').innerHTML;
-    }
-    givenAns.innerHTML = myChosenAnswers;
+  //convert random generated binary to equivalent decial value
+  if (binar[i] == 1) {
+    decimVal = decimVal + Math.pow(2, 7 - i);
+  } else {
+    decimVal += 0;
+  }
 }
 
-/*//////////FUNCTIONS/////////*/
+const makeItHidden = () => {
+  //function for hidding the clicked cell table row
+  clickedObjectContainer.style.display = "none";
+}; //hiding
+
+const testForHidingRow = () => {
+  //hiding the whole row in which all cells were clicked
+  let tablRowCount = document.querySelector(".answersTable").rows.length;
+  console.log(tablRowCount);
+  for (var Indx = 1; Indx < tablRowCount + 1; Indx++) {
+    if (
+      document.querySelector(
+        ".answersTable tr:nth-child(" + Indx + ") td:nth-child(2)>a"
+      ).style.display == "none" &&
+      document.querySelector(
+        ".answersTable tr:nth-child(" + Indx + ") td:nth-child(3)>a"
+      ).style.display == "none"
+    ) {
+      document.querySelector(
+        ".answersTable tr:nth-child(" + Indx + ")"
+      ).style.display = "none";
+    } //end of if
+  } //end of loop
+}; //end of function hiding rows
+
+const populatGivenAns = () => {
+  if (myChosenAnswers == "") {
+    myChosenAnswers = clickedObjectContainer.querySelector("div").innerHTML;
+  }
+  givenAns.innerHTML = myChosenAnswers;
+};
+
 const myArray = [decimVal]; //*********defining array of answers
 
 //adding other false random answers to myArray
 for (let j = 0; j < 5; j++) {
-    let randNum = Math.floor(Math.random() * 256);
-    console.log(randNum);
-    myArray.push(randNum);
+  let randNum = Math.floor(Math.random() * 256);
+  console.log(randNum);
+  myArray.push(randNum);
 }
 
-function shuffleArray(myArray) {
-    for (var i = myArray.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = myArray[i];
-        myArray[i] = myArray[j];
-        myArray[j] = temp;
-    }
-    return myArray;
-}
+const shuffleArray = (myArray) => {
+  for (var i = myArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = myArray[i];
+    myArray[i] = myArray[j];
+    myArray[j] = temp;
+  }
+  return myArray;
+};
 var newArray = shuffleArray(myArray);
-// console.log(newArray);
-//*********************checking the shuffled array for empty elements
-// function testForempty(newArray) {
-//     return newArray != null;
-// }
 
-// console.log(newArray);
-/*********************************************End of random shuffling of the options**************/
-/*/\/\/\/\Checking the matching with correct answer/\/\/\/\*/
+let t = 0;
+let timerNum = setInterval(() => {
+  //function for counting up to 30 seconds
+  if (t < 30) {
+    t++;
+    document.getElementById("timer-value").innerHTML = t;
+  } else {
+    clearInterval(timerNum);
+    document.getElementById("timer-value").innerHTML = "Time is up!";
+    document.querySelector(".timer-wraper").style.display = "none";
+    document.getElementById("timeUp").style.display = "block";
+  }
+}, 1000);
 /*******************timer*********************** */
-let t = 0
-let timerNum = setInterval(printSec, 1000);
-// console.log(timerNum);
-function printSec() {
-    if (t < 30) {
-        t++;
-        document.getElementById('timer-value').innerHTML = t;
+const runToCheck = () => {
+  document.getElementById("timer-value").innerHTML = t;
+  clearInterval(timerNum); //capturing the latest elapsed time
+  let resultAns = document.getElementById("matchField");
+  const myFunctionCorrect = () => {
+    resultAns.innerHTML = "Correct!";
+    document.body.style.backgroundColor = "#96E29A"; //color feedback correct
+    givenAns.style.color = "#22E02D";
+  }; //end of function myFunctionCorrect
+  //~~~~~~~~~~~~~~~~function definition for False
+  const myFunctionFalse = () => {
+    //injecting a class for changing the color of the FALSE text to red
+    resultAns.innerHTML = "False!";
+    document.body.style.backgroundColor = "#FF5760"; //color feedback correct
+    resultAns.style.color = "#FF0000";
+    if (myChosenAnswers == "") {
+      givenAns.innerHTML = `Correct answer is: ${decimVal}, and you didn't choose an option`;
     } else {
-        clearInterval(timerNum);
-        // console.log(`done!!`);
-        document.getElementById('timer-value').innerHTML = 'Time is up!';
-        document.querySelector(".timer-wraper").style.display = "none";
-        document.getElementById("timeUp").style.display = "block";
+      givenAns.innerHTML = `Correct answer is: ${decimVal}, You Chose ${myChosenAnswers}!`;
     }
-}
-/*******************timer*********************** */
-function runToCheck() {
-    // let hidTable = document.querySelector('.answersTable').style.display = 'none';
-    //~~~~~~~~~~~~~~~~function definition for correct
-    let resultAns = document.getElementById('matchField');
-    function myFunctionCorrect() {
-        resultAns.innerHTML = 'Correct!';
-        document.body.style.backgroundColor = '#96E29A'; //color feedback correct
-        givenAns.style.color = '#22E02D';
-    } //end of function myFunctionCorrect
-    //~~~~~~~~~~~~~~~~function definition for False
-    function myFunctionFalse() {
-        //injecting a class for changing the color of the FALSE text to red
-        resultAns.innerHTML = 'False!';
-        document.body.style.backgroundColor = '#FF5760'; //color feedback correct
-        resultAns.style.color = '#FF0000';
-        if (myChosenAnswers == '') {
-            givenAns.innerHTML = `Correct answer is: ${decimVal}, and you didn't choose an option`
-        } else {
-            givenAns.innerHTML = `Correct answer is: ${decimVal}, You Chose ${myChosenAnswers}!`;
-        }
-    } //end of function myFunctionFalse
-    //~~~~~~~~~~~~~~~~Main condition~~~~~~~~~~~~~////////////////////////////////
-    let cmbAnsFieldContent = givenAns.innerText;
-    if (clickedObjectContainer != undefined) {
-        //document.getElementById('askedQuestion').innerHTML = ansCombinedWithSingleQues;
-        if (cmbAnsFieldContent == decimVal) {
-            myFunctionCorrect();
-        } else {
-            myFunctionFalse();
-        }
-
+  }; //end of function myFunctionFalse
+  let cmbAnsFieldContent = givenAns.innerText;
+  if (clickedObjectContainer != undefined) {
+    if (cmbAnsFieldContent == decimVal) {
+      myFunctionCorrect();
     } else {
-        myFunctionFalse();
+      myFunctionFalse();
     }
-} //end of function runToCheck
-
-/*▐▐▐▐▐▐▐▐▐Assigning shuffled new array elements to HTML cells in the table▐▐▐▐▐▐▐▐▐▐▐*/
+  } else {
+    myFunctionFalse();
+  }
+}; //end of function runToCheck
 for (let q = 0; q < newArray.length; q++) {
-    var indexStr = String(q + 1);
-    // console.log(newArray.length);
-    indexStr = 'print_arr' + indexStr;
-    // console.log(indexStr);
-    document.getElementById(indexStr).innerHTML = newArray[q];
-
+  var indexStr = String(q + 1);
+  // console.log(newArray.length);
+  indexStr = "print_arr" + indexStr;
+  // console.log(indexStr);
+  document.getElementById(indexStr).innerHTML = newArray[q];
 }
